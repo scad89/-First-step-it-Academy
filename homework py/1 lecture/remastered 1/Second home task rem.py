2a7aaf2efda9cba79cbb40a45810531c007ad7ee
scad89 <71154471+scad89@users.noreply.github.com>
2020-09-28 22:51:37 +0300

    Remastered2 homework for №1 lecture Python with opt

--- homework py/1 lecture/remastered 1/Second home task rem.py
+++ homework py/1 lecture/remastered 1/Second home task rem.py
@@ -1,5 +1,8 @@
 # 1-ый способ
 
+import itertools
+
+
 def list():
     try:
         a = int(input('Сколько элементов содержит список? '))
@@ -24,3 +27,29 @@ def target():
 
 
 target()
+
+
+# 2-ой способ(оптимизация)
+
+
+def list():
+    try:
+        a = int(input('Сколько элементов содержит список? '))
+        list1 = []
+    except ValueError:
+        return
+    for _ in range(a):
+        a1 = int(input())
+        list1.append(a1)
+    return list1
+
+
+def target():
+
+    b = list()
+    tar = int(input("Target: "))
+    comb = itertools.combinations(range(len(b)), 2)
+    for i, j in comb:
+        if b[i] + b[j] == tar:
+            print(i, j)
+            return
