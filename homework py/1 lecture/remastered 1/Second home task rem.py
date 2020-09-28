ac18d2709a1fdacbea4750fc76b51ede161bcbd6
scad89 <71154471+scad89@users.noreply.github.com>
2020-09-28 21:54:09 +0300

    Remastered2 homework for №1 lecture Python

--- homework py/1 lecture/remastered 1/Second home task rem.py
+++ homework py/1 lecture/remastered 1/Second home task rem.py
@@ -1,4 +1,4 @@
-# этот способ решает [2, 7, 11, 15] target = 9 и [3, 2, 4] target = 6:
+# 1-ый способ
 
 def list():
     try:
@@ -16,39 +16,10 @@ def target():
 
     b = list()
     tar = int(input("Target: "))
-    for i in b:
-        for j in b:
-            if i+j == tar:
-                print(b.index(i), b.index(j))
-                return
-
-
-target()
-
-
-# а этот [3, 3] target = 6
-
-def list():
-    try:
-        a = int(input('Сколько элементов содержит список? '))
-        list1 = []
-    except ValueError:
-        return
-    for _ in range(a):
-        a1 = int(input())
-        list1.append(a1)
-    return list1
-
-
-def target():
-
-    b = list()
-    tar = int(input("Target: "))
-    for i in b:
-        for j in b[1:]:
-            if i+j == tar:
-                right_index = len(b) - b[::-1].index(i) - 1
-                print(b.index(i), right_index)
+    for i in range(len(b)-1):
+        for j in range(i+1, len(b)):
+            if b[i]+b[j] == tar:
+                print(i, j)
                 return
 
 
