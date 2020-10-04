c5fb7672bdc1fd3c6427cf2f696adfdb0f18e816
Ihar <71154471+scad89@users.noreply.github.com>
2020-10-05 00:40:41 +0300

    Update 1 home task.py

--- homework py/2 lecture/1 home task.py
+++ homework py/2 lecture/1 home task.py
@@ -5,8 +5,7 @@ def change(tuples):
             n_list[i] = n_list[i]*2
         elif i % 2 != 0:
             n_list[i] = n_list[i]-2
-    tuples_ch = tuple(n_list)
-    return tuples_ch
+    return tuple(n_list)
 
 
 tuples = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
