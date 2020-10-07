2e83a805690b446841b874913a6ec9633a400e01
scad89 <71154471+scad89@users.noreply.github.com>
2020-10-07 22:19:40 +0300

    complete

---
+++ homework py/3 lecture/1_task_hw_3_lecture.py
@@ -0,0 +1,7 @@
+list = [i for i in range(1, 100000)]
+double_list = []
+for i in list[:: 2]:
+    if i % 3 != 0 and i % 5 != 0:
+        double_list.append(i)
+double_list.insert(1, 2)
+print(double_list)
