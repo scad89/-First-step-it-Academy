5fa40ebe488e44fa07aae3676d1eb9bc11e5c088
scad89 <71154471+scad89@users.noreply.github.com>
2020-10-21 03:13:44 +0300

    6th optimize 3 task

--- src/1 lecture/3_task_hw_1_lecture_rem.py
+++ src/1 lecture/3_task_hw_1_lecture_rem.py
@@ -1,12 +1,10 @@
 def find_double_ints(ls):
 
     ls.sort()
-    num = 0
-    for i in ls:
-        if i ^ num == 0:
-            print(f'Double int {i}')
-        else:
-            num = i
+    for i in range(len(ls)):
+        if ls[i] ^ ls[i+1] == 0:
+            print(f'Double int {ls[i]}')
+            break
 
 
 ls = []
